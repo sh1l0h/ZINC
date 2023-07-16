1d1a376f980970409e44da9333bd8287a9939959
Mirian Shilakadze <mirian.shilakadze@gmail.com>
2023-07-16 17:34:42 +0400

    Add zinc.h

---
+++ include/zinc.h
@@ -0,0 +1,18 @@
+#ifndef ZINC_H
+#define ZINC_H
+
+#include "./vec2.h"
+#include "./vec3.h"
+#include "./vec4.h"
+#include "./vec2i.h"
+#include "./vec3i.h"
+#include "./vec4i.h"
+#include "./mat2.h"
+#include "./mat3.h"
+#include "./mat4.h"
+#include "./io.h"
+#include "./types.h"
+#include "./camera.h"
+#include "./transformation.h"
+
+#endif
